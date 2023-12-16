9b9e453f0724fc9265943f8cfded8d1b339f15fa
sangchu <didi1484@gmail.com>
2023-12-16 16:14:15 +0900

    feat: 사원 객체 생성 기능 추가

--- src/main/java/oncall/controller/Controller.java
+++ src/main/java/oncall/controller/Controller.java
@@ -1,5 +1,9 @@
 package oncall.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import oncall.domain.Employee;
+import oncall.domain.EmployeeGroup;
 import oncall.domain.WorkDays;
 import oncall.view.InputView;
 import oncall.view.OutputView;
@@ -15,14 +19,45 @@ public class Controller {
 
     public void start() {
         WorkDays workDays = createWorkDays();
+        EmployeeGroup weekdayEmployees = createWeekdayEmployees();
+        EmployeeGroup weekendEmployees = createWeekendEmployees();
     }
+
 
     private WorkDays createWorkDays() {
         while (true) {
             try {
                 String input = inputView.readDays();
                 return new WorkDays(input);
-            } catch (Exception e) {
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private EmployeeGroup createWeekdayEmployees() {
+        while (true) {
+            try {
+                List<String> input = inputView.readWeekdayEmployees();
+                List<Employee> employees = input.stream()
+                        .map(Employee::new)
+                        .collect(Collectors.toList());
+                return new EmployeeGroup(employees);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e);
+            }
+        }
+    }
+
+    private EmployeeGroup createWeekendEmployees() {
+        while (true) {
+            try {
+                List<String> input = inputView.readWeekdayEmployees();
+                List<Employee> employees = input.stream()
+                        .map(Employee::new)
+                        .collect(Collectors.toList());
+                return new EmployeeGroup(employees);
+            } catch (IllegalArgumentException e) {
                 outputView.printErrorMessage(e);
             }
         }
